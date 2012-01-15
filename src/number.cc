07395c6ac799284803312818f21577f218f00974
y2q <y2q.actionman@gmail.com>
2012-01-15 20:48:20 +0900

    fixed decimal impl

--- src/number.cc
+++ src/number.cc
@@ -2,6 +2,7 @@
 #include <utility>
 #include <cctype>
 #include <sstream>
+#include <cstdlib>
 
 #include "number.hh"
 
@@ -37,7 +38,6 @@ pair<int, Exactness> parse_number_prefix(std::istream& i){
 
   if(i.peek() != '#')
     return make_pair(r, e);
-
   i.ignore(1);
 
   switch(auto c = i.get()){
@@ -50,7 +50,7 @@ pair<int, Exactness> parse_number_prefix(std::istream& i){
     r_appeared = true;
     break;
   default:
-    throw "informal char appeared!";
+    goto error;
   }
   
   if(i.peek() != '#')
@@ -59,115 +59,112 @@ pair<int, Exactness> parse_number_prefix(std::istream& i){
 
   switch(auto c = i.get()){
   case 'i': case 'e':
-    if(e_appeared) throw "exactness specified twice";
+    if(e_appeared) goto error;
     e = to_exactness(c);
     break;
   case 'b': case 'o': case 'd': case 'x':
-    if(r_appeared) throw "radix specified twice";
+    if(r_appeared) goto error;
     r = to_radix(c);
   default:
-    throw "informal char appeared!";
+    goto error;
   }
   
   return make_pair(r, e);
+
+ error:
+  return make_pair(-1, Exactness::unspecified);
 }
 
-
-static inline
-int char_to_int(char c){
-  switch(c){
-  case '0': return 0;
-  case '1': return 1;
-  case '2': return 2;
-  case '3': return 3;
-  case '4': return 4;
-  case '5': return 5;
-  case '6': return 6;
-  case '7': return 7;
-  case '8': return 8;
-  case '9': return 9;
-  case 'a': case 'A': return 10;
-  case 'b': case 'B': return 11;
-  case 'c': case 'C': return 12;
-  case 'd': case 'D': return 13;
-  case 'e': case 'E': return 14;
-  case 'f': case 'F': return 15;
-  default: return -1;
-  }
-}
 
 template<int radix>
 struct is_number_char{
   template<typename CharT>
   inline
-  int operator()(CharT c) const;
+  bool operator()(CharT c) const;
 };
 
 template<>
 template<typename CharT>
 inline
-int is_number_char<2>::operator()(CharT c) const{
+bool is_number_char<2>::operator()(CharT c) const{
   switch(c){
   case '0': case '1':
-    return char_to_int(c);
+    return true;
   default:
-    return -1;
+    return false;
   }
 }
 
 template<>
 template<typename CharT>
 inline
-int is_number_char<8>::operator()(CharT c) const{
+bool is_number_char<8>::operator()(CharT c) const{
   switch(c){
   case '0': case '1': case '2': case '3':
   case '4': case '5': case '6': case '7':
-    return char_to_int(c);
+    return true;
   default:
-    return -1;
+    return false;
   }
 }
 
 template<>
 template<typename CharT>
 inline
-int is_number_char<10>::operator()(CharT c) const{
-  return isdigit(c) ? char_to_int(c) : -1;
+bool is_number_char<10>::operator()(CharT c) const{
+  return isdigit(c);
 }
 
 template<>
 template<typename CharT>
 inline
-int is_number_char<16>::operator()(CharT c) const{
-  return isxdigit(c) ? char_to_int(c) : -1;
+bool is_number_char<16>::operator()(CharT c) const{
+  return isxdigit(c);
 }
 
+template<typename T>
+static inline
+int ignore_sharp(T& i){
+  int sharps = 0;
+
+  while(i.peek() == '#'){
+    i.ignore(1);
+    ++sharps;
+  }
+
+  return sharps;
+}
+
+static inline
+bool is_inited(const Number& n){
+  return n.type() != Number::Type::uninitialized;
+}
 
 template<int radix>
 static
 Number parse_unsigned(std::istream& i){
-  auto fun = is_number_char<radix>{};
-  unsigned long ret = 0;
+  static const auto fun = is_number_char<radix>{};
+  const auto pos = i.tellg();
+  stringstream s;
 
-  while(1){
-    ret *= radix;
+  while(fun(i.peek()))
+    s.put(i.get());
 
-    auto n = fun(i.peek());
-    if(n < 0){
-      break;
-    }else{
-      i.ignore(1);
-      ret += n;
-    }
+  if(s.str().empty()){
+    goto error;
+  }else{
+    errno = 0;
+    long l = strtol(s.str().c_str(), nullptr, radix);
+    if(errno) goto error;
+
+    ignore_sharp(i);
+
+    return Number{l};
   }
 
-  while(1){
-    auto c = i.peek();
-    if(c != '#') break;
-    i.ignore(1);
-  }
-
-  return Number{static_cast<long>(ret)};
+ error:
+  i.seekg(pos);
+  return Number{};
 }
 
 template<typename CharT>
@@ -186,27 +183,15 @@ Number parse_decimal(std::istream& i){
   static const auto read_char_func = is_number_char<10>{};
   const auto pos = i.tellg();
 
-  const auto ignore_sharp = [&]() -> int{
-    int sharps = 0;
-    
-    while(i.peek() == '#'){
-      i.ignore(1);
-      ++sharps;
-    }
-
-    return sharps;
-  };
-  
   stringstream s;
   decltype(i.peek()) c;
   
-  // int_part
   while((c = read_char_func(i.peek())) >= 0){
     s.put(i.get());
   }
 
   bool dot_start = false;
-  int sharps = 0;
+  int sharps_before_dot = 0;
 
   if(s.str().empty()){
     if(i.peek() == '.'){
@@ -215,43 +200,48 @@ Number parse_decimal(std::istream& i){
       goto error;
     }
   }else{
-    sharps = ignore_sharp();
+    sharps_before_dot = ignore_sharp(i);
   }
 
   if(i.peek() != '.'){
-    if(check_decimal_suffix(i.peek()))
-      goto end;
-    else
-      goto error;
+    goto end; // 1. no frac part
   }
   s.put(i.get());
     
-  if(sharps > 0){
-    ignore_sharp();
-
-    if(check_decimal_suffix(i.peek()))
-      goto end;
-    else
-      goto error;
+  if(sharps_before_dot > 0){
+    ignore_sharp(i);
+    goto end; // 4. no frac -- sharps before dot
   }
 
   if(dot_start && read_char_func(i.peek()) < 0)
-    goto error;
+    goto error; // 2. dot start should have digits
 
   while((c = read_char_func(i.peek())) >= 0){
     s.put(i.get());
   }
 
-  ignore_sharp();
+  ignore_sharp(i);
   
-  if(read_char_func(i.peek()) < 0)
-    goto error;
+ end:
+  if(check_decimal_suffix(i.peek()) < 0){
+    i.ignore(1);
+    s.put('e');
 
- end: {
-    double d;
-    s >> d;
-    return Number{d};
+    switch(i.peek()){
+    case '+': case '-':
+      s.put(i.get());
+    }
+
+    if(read_char_func(i.peek()) < 0){
+      goto error; // no number on exp. part
+    }
+
+    while((c = read_char_func(i.peek())) >= 0){
+      s.put(i.get());
+    }
   }
+
+  return Number{strtod(s.str().c_str(), nullptr)};
 
  error:
   i.seekg(pos);
@@ -279,7 +269,10 @@ Number parse_real_number(std::istream& i){
   }
 
   auto u1 = parse_unsigned<radix>(i);
+  if(!is_inited(u1))
+    goto error;
 
+  // decimal float
   if(radix == 10 && check_decimal_suffix(i.peek())){
     i.seekg(pos);
     auto n = parse_decimal(i);
@@ -290,18 +283,26 @@ Number parse_real_number(std::istream& i){
   }
 
   if(i.peek() != '/'){
+  // integer
     return Number(sign * u1.get<long>());
   }else{
+    // rational
     auto u2 = parse_unsigned<radix>(i);
+    if(!is_inited(u2))
+      goto error;
     return Number(sign * u1.get<double>() / u2.get<double>());
   }
+
+ error:
+  i.seekg(pos);
+  return Number{};
 }
 
 template<int radix>
 static
 Number parse_complex(std::istream& i){
-  auto imag_parser = [&](const Number& real,
-                         decltype(i.peek()) sign_char) -> Number {
+  const auto imag_parser = [&](const Number& real,
+                               decltype(i.peek()) sign_char) -> Number {
     const int sign = (sign_char == '+') ? 1 : -1;
 
     i.ignore(1);
@@ -312,22 +313,20 @@ Number parse_complex(std::istream& i){
     
     Number imag = parse_unsigned<radix>(i);
 
-    if(imag.type() == Number::Type::uninitialized
-       || i.peek() != 'i')
+    if(!is_inited(imag) || i.peek() != 'i')
       return Number{}; // error
 
     return Number{Number::complex_type{real.get<double>(), imag.get<double>() * sign}};
   };
 
+  const auto pos = i.tellg();
   auto first_char = i.peek();
 
   // no real part
   if(first_char == '+' || first_char == '-'){
-    auto pos = i.tellg();
-
     Number n = imag_parser(Number{static_cast<long>(0)}, first_char);
 
-    if(n.type() != Number::Type::uninitialized)
+    if(is_inited(n))
       return n;
 
     i.seekg(pos);
@@ -335,16 +334,16 @@ Number parse_complex(std::istream& i){
 
   // has real part
   Number real = parse_real_number<radix>(i);
-  if(real.type() == Number::Type::uninitialized)
-    return Number{};
+  if(!is_inited(real))
+    goto error;
 
   switch(auto c = i.peek()){
   case '@': {// polar literal
     i.ignore(1);
     Number deg = parse_real_number<radix>(i);
 
-    if(real.type() == Number::Type::uninitialized)
-      return Number{};
+    if(!is_inited(deg))
+      goto error;
         
     return Number{polar(real.get<double>(), deg.get<double>())};
   }
@@ -353,14 +352,18 @@ Number parse_complex(std::istream& i){
   default:
     return real;
   }
+
+ error:
+  i.seekg(pos);
+  return Number{};
 }
 
 Number parse_number(std::istream& i){
-  auto prefix_info = parse_number_prefix(i);
+  const auto prefix_info = parse_number_prefix(i);
 
   Number n;
 
-  switch(prefix_info.first){
+  switch(get<0>(prefix_info)){
   case 2:
     n = parse_complex<2>(i);
     break;
@@ -373,10 +376,10 @@ Number parse_number(std::istream& i){
   case 16:
     n = parse_complex<16>(i);
   default:
-    throw "internal parser error!";
+    return Number{};
   }
 
-  switch(prefix_info.second){
+  switch(get<1>(prefix_info)){
   case Exactness::exact:
     return to_exact(n);
   case Exactness::inexact:
@@ -384,6 +387,6 @@ Number parse_number(std::istream& i){
   case Exactness::unspecified:
     return n;
   default:
-    throw "internal parser error!";
+    return Number{};
   }
 }
