07c5ed6d05619555b80326037007b2040daf35f6
Y.Y <y2q.actionman@gmail.com>
2012-03-16 11:38:22 +0900

    added function param

--- src/function.hh
+++ src/function.hh
@@ -7,8 +7,19 @@ class Stack;
 
 class Function {
 public:
-  explicit Function(Lisp_ptr code)
-    : code_(code){}
+  typedef Lisp_ptr(*NativeFunc)(Env&, Stack&);
+
+  enum class Type {
+    interpreted,
+    native
+  };
+
+
+  explicit Function(Lisp_ptr code, int args)
+    : type_(Type::interpreted), args_(args), code_(code){}
+  explicit Function(NativeFunc func, int args)
+    : type_(Type::native), args_(args), n_func_(func){}
+
   
   Function(const Function&) = default;
   Function(Function&&) = default;
@@ -16,11 +27,17 @@ public:
   Function& operator=(const Function&) = default;
   Function& operator=(Function&&) = default;
 
-  Lisp_ptr call(Env&, Stack&);
   Lisp_ptr call(Env&, Stack&, Lisp_ptr args);
 
 private:
-  Lisp_ptr code_;
+  Type type_; // TODO: merge these two fields into bitfields
+  int args_;
+  union{
+    Lisp_ptr code_;
+    NativeFunc n_func_;
+  };
+
+  Lisp_ptr call(Env&, Stack&);
 };
 
 Function* make_function(Lisp_ptr args, Cons* code);
