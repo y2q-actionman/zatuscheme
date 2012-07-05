0e2e89e37e03a77ca56a2429fc4b989c8507fad0
y2q <y2q.actionman@gmail.com>
2012-07-06 05:22:36 +0900

    cleanup

--- src/builtin_util.hh
+++ src/builtin_util.hh
@@ -1,119 +1,27 @@
 #ifndef BUILTIN_UTIL_HH
 #define BUILTIN_UTIL_HH
 
-#include <stack>
-#include <vector>
 #include <array>
-#include <cstdio>
-
 #include "lisp_ptr.hh"
-#include "cons.hh"
-#include "vm.hh"
 
 template<typename StackT>
-Lisp_ptr stack_to_list(StackT& st, bool dot_list){
-  Cons* c = new Cons;
-  Cons* prev_c = c;
-  Lisp_ptr ret = Lisp_ptr{c};
-
-  while(1){
-    c->rplaca(st.top());
-    st.pop();
-
-    if(st.top().tag() == Ptr_tag::vm_op){
-      st.pop();
-      break;
-    }
-
-    Cons* newc = new Cons;
-    c->rplacd(Lisp_ptr(newc));
-    prev_c = c;
-    c = newc;
-  }
-
-  if(dot_list){
-    if(c != prev_c){
-      prev_c->rplacd(c->car());
-    }else{
-      ret = c->car();
-    }
-    delete c;
-  }else{
-    c->rplacd(Cons::NIL);
-  }
-
-  return ret;
-}
+Lisp_ptr stack_to_list(StackT&, bool);
 
 template<typename StackT, typename VectorT>
-void stack_to_vector(StackT& st, VectorT& v){
-  while(1){
-    v.push_back(st.top());
-    st.pop();
-
-    if(st.top().tag() == Ptr_tag::vm_op){
-      st.pop();
-      break;
-    }
-  }
-}
+void stack_to_vector(StackT&, VectorT&);
 
 template<typename StackT>
-int list_to_stack(const char* opname, Lisp_ptr l, StackT& st){
-  std::stack<Lisp_ptr, std::vector<Lisp_ptr>> tmp;
-  
-  do_list(l,
-          [&](Cons* c) -> bool {
-            tmp.push(c->car());
-            return true;
-          },
-          [&](Lisp_ptr last_cdr){
-            if(!nullp(last_cdr)){
-              fprintf(stderr, "eval warning: dot list has read as proper list. (in %s)\n",
-                      opname);
-              tmp.push(last_cdr);
-            }
-          });
-
-  int ret = 0;
-
-  while(!tmp.empty()){
-    st.push(tmp.top());
-    tmp.pop();
-    ++ret;
-  }
-
-  return ret;
-}  
+int list_to_stack(const char*, Lisp_ptr, StackT&);
 
 template<int i>
-std::array<Lisp_ptr, i> pick_args(){
-  auto ret = std::array<Lisp_ptr, i>();
-
-  for(auto it = ret.rbegin(); it != ret.rend(); ++it){
-    if(VM.stack().empty()) goto error;
-
-    *it = VM.stack().top();
-    VM.stack().pop();
-  }
-
-  if(VM.stack().empty()
-     || VM.stack().top().tag() != Ptr_tag::vm_op)
-    goto error;
-  VM.stack().pop(); // kill arg_bottom
-
-  return ret;
-
- error:
-  fprintf(stderr, "eval error: stack corruption.\n");
-  ret.fill({});
-  return ret;
-}
+std::array<Lisp_ptr, i> pick_args();
 
 
 // some builtin functions
 void procedure_list();
 void procedure_list_star();
 void procedure_vector();
+
+#include "builtin_util.i.hh"
 
 #endif //BUILTIN_UTIL_HH
---
+++ src/builtin_util.i.hh
@@ -0,0 +1,117 @@
+#ifndef BUILTIN_UTIL_I_HH
+#define BUILTIN_UTIL_I_HH
+
+#ifndef BUILTIN_UTIL_HH
+#error "Please include via parent file"
+#endif
+
+#include <stack>
+#include <vector>
+#include <array>
+#include <cstdio>
+
+#include "lisp_ptr.hh"
+#include "cons.hh"
+#include "vm.hh"
+
+template<typename StackT>
+Lisp_ptr stack_to_list(StackT& st, bool dot_list){
+  Cons* c = new Cons;
+  Cons* prev_c = c;
+  Lisp_ptr ret = Lisp_ptr{c};
+
+  while(1){
+    c->rplaca(st.top());
+    st.pop();
+
+    if(st.top().tag() == Ptr_tag::vm_op){
+      st.pop();
+      break;
+    }
+
+    Cons* newc = new Cons;
+    c->rplacd(Lisp_ptr(newc));
+    prev_c = c;
+    c = newc;
+  }
+
+  if(dot_list){
+    if(c != prev_c){
+      prev_c->rplacd(c->car());
+    }else{
+      ret = c->car();
+    }
+    delete c;
+  }else{
+    c->rplacd(Cons::NIL);
+  }
+
+  return ret;
+}
+
+template<typename StackT, typename VectorT>
+void stack_to_vector(StackT& st, VectorT& v){
+  while(1){
+    v.push_back(st.top());
+    st.pop();
+
+    if(st.top().tag() == Ptr_tag::vm_op){
+      st.pop();
+      break;
+    }
+  }
+}
+
+template<typename StackT>
+int list_to_stack(const char* opname, Lisp_ptr l, StackT& st){
+  std::stack<Lisp_ptr, std::vector<Lisp_ptr>> tmp;
+  
+  do_list(l,
+          [&](Cons* c) -> bool {
+            tmp.push(c->car());
+            return true;
+          },
+          [&](Lisp_ptr last_cdr){
+            if(!nullp(last_cdr)){
+              fprintf(stderr, "eval warning: dot list has read as proper list. (in %s)\n",
+                      opname);
+              tmp.push(last_cdr);
+            }
+          });
+
+  int ret = 0;
+
+  while(!tmp.empty()){
+    st.push(tmp.top());
+    tmp.pop();
+    ++ret;
+  }
+
+  return ret;
+}  
+
+template<int i>
+std::array<Lisp_ptr, i> pick_args(){
+  auto ret = std::array<Lisp_ptr, i>();
+
+  for(auto it = ret.rbegin(); it != ret.rend(); ++it){
+    if(VM.stack().empty()) goto error;
+
+    *it = VM.stack().top();
+    VM.stack().pop();
+  }
+
+  if(VM.stack().empty()
+     || VM.stack().top().tag() != Ptr_tag::vm_op)
+    goto error;
+  VM.stack().pop(); // kill arg_bottom
+
+  return ret;
+
+ error:
+  fprintf(stderr, "eval error: stack corruption.\n");
+  ret.fill({});
+  return ret;
+}
+
+#endif //BUILTIN_UTIL_I_HH
