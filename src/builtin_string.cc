7b1fc2a1ed89d26203656452df6d1879c6f22dac
y2q <y2q.actionman@gmail.com>
2012-10-03 22:44:39 +0900

    added 2 funcs

--- src/builtin_string.cc
+++ src/builtin_string.cc
@@ -30,6 +30,7 @@ void string_make(){
   if(num->type() != Number::Type::integer){
     fprintf(zs::err, "native func: make-string: arg's number is not %s! (%s)\n",
             stringify(Number::Type::integer), stringify(num->type()));
+    VM.return_value = {};
     clean_args();
     return;
   }
@@ -71,6 +72,49 @@ void string_string(){
   VM.return_value = {new String(std::move(ret))};
 }
   
+void string_length(){
+  auto arg1 = pick_args_1();
+  auto str = arg1.get<String*>();
+  if(!str){
+    string_type_check_failed("string-length", arg1);
+    return;
+  }
+
+  VM.return_value = {new Number(static_cast<Number::integer_type>(str->length()))};
+}
+
+void string_ref(){
+  auto arg = pick_args<2>();
+  auto str = arg[0].get<String*>();
+  if(!str){
+    string_type_check_failed("string-ref", arg[0]);
+    return;
+  }
+
+  auto num = arg[1].get<Number*>();
+  if(!num){
+    builtin_type_check_failed("string-ref", Ptr_tag::number, arg[1]);
+    return;
+  }
+
+  if(num->type() != Number::Type::integer){
+    fprintf(zs::err, "native func: string-ref: arg's number is not %s! (%s)\n",
+            stringify(Number::Type::integer), stringify(num->type()));
+    VM.return_value = {};
+    return;
+  }
+  auto ind = num->get<Number::integer_type>();
+
+  if(ind < 0 || ind >= str->length()){
+    fprintf(zs::err, "native func: string-ref: index is out-of-bound ([0, %ld), supplied %ld\n",
+            str->length(), ind);
+    VM.return_value = {};
+    return;
+  }
+
+  VM.return_value = Lisp_ptr{str->operator[](ind)};
+}
+
 } // namespace
 
 const BuiltinFunc
@@ -84,6 +128,12 @@ builtin_string[] = {
   {"string", {
       string_string,
       {Calling::function, 0, Variadic::t}}},
+  {"string-length", {
+      string_length,
+      {Calling::function, 1}}},
+  {"string-ref", {
+      string_ref,
+      {Calling::function, 2}}},
 };
 
 const size_t builtin_string_size = sizeof(builtin_string) / sizeof(builtin_string[0]);
--- test/builtin_string_test.cc
+++ test/builtin_string_test.cc
@@ -7,6 +7,10 @@ void check(const char* input, const char* expect){
   result = read_eval_print_test(input, expect);
 }
 
+void check_undef(const char* input){
+  result = !eval_text(input);
+}
+
 // TODO: if case-insensitivity is supported, define CASE_INSENSITIVE
 
 
@@ -21,6 +25,18 @@ int main(){
   check("(string #\\a)", "\"a\"");
   check("(string #\\a #\\b #\\c)", "\"abc\"");
   
+  check("(string-length \"\")", "0");
+  check("(string-length \"a\")", "1");
+  check("(string-length \"abcde\")", "5");
+
+  check("(string-ref \"a\" 0)", "#\\a");
+  check("(string-ref \"abcde\" 3)", "#\\d");
+  {
+    with_null_stream wns;
+    check_undef("(string-ref \"a\" -1)");
+    check_undef("(string-ref \"a\" 100)");
+    check_undef("(string-ref \"\" 0)");
+  }
 
   return (result) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
