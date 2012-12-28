c8bf1b6296a012e0145a3371bf824947be31bae5
y2q <y2q.actionman@gmail.com>
2012-12-28 16:36:13 +0900

    reformed eval_test.cc

--- test/eval_test.cc
+++ test/eval_test.cc
@@ -31,18 +31,6 @@ bool read_eqv(Lisp_ptr input, const char* expect_s){
   return eqv(input, expect);
 }
 
-bool print_equal(Lisp_ptr input, const char* expect_s){
-  const auto callback = [expect_s](const char* str){
-    cerr << "[failed] expected: " << expect_s << "\n\tevaled: " << str << "\n";
-  };
-
-  return test_on_print(input, expect_s, callback);
-}
-
-bool test_undef(Lisp_ptr p, const char*){
-  return !p;
-}
-
 bool test_true(Lisp_ptr p, const char*){
   return !!p;
 }
@@ -62,11 +50,11 @@ int main(){
   check(read_eqv, "#\\R", "#\\R");
   check(read_eqv, "#\\Newline", "#\\Newline");
 
-  check(print_equal, "\"sss\"", "\"sss\"");
-  check(print_equal, "\"\"", "\"\"");
+  check_e("\"sss\"", "\"sss\"");
+  check_e("\"\"", "\"\"");
 
-  check(print_equal, "#(1 2 3)", "#(1 2 3)");
-  check(print_equal, "#(1 #(11 12 13) 3)", "#(1 #(11 12 13) 3)");
+  check_e("#(1 2 3)", "#(1 2 3)");
+  check_e("#(1 #(11 12 13) 3)", "#(1 #(11 12 13) 3)");
 
   check(read_eqv, "()", "()");
 
@@ -74,9 +62,7 @@ int main(){
 
 
   // === symbol-value ===
-  with_expect_error([]() -> void {
-      check(test_undef, "tabun-tukattenai-namae");
-    });
+  check_e_undef("tabun-tukattenai-namae");
 
 
   // === function call ===
@@ -85,29 +71,21 @@ int main(){
 
   // === Special Operator ===
   // syntax: quote
-  with_expect_error([]() -> void {
-      check(test_undef, "(quote)");
-    });
+  check_e_undef("(quote)");
   check(read_eqv, "(quote 1)", "1");
-  check(print_equal, "(quote (1 . 2))", "(1 . 2)");
-  check(print_equal, "'(1 2 3)", "(1 2 3)");
+  check_e("(quote (1 . 2))", "(1 . 2)");
+  check_e("'(1 2 3)", "(1 2 3)");
 
   // syntax: lambda
 
   // syntax: if
-  with_expect_error([]() -> void {
-      check(test_undef, "(if)");
-      check(test_undef, "(if 1)");
-    });
+  check_e_undef("(if)");
+  check_e_undef("(if 1)");
   check(read_eqv, "(if #t 1)", "1");
-  with_expect_error([]() -> void {
-      check(test_undef, "(if #f 1)");
-    });
+  check_e_undef("(if #f 1)");
   check(read_eqv, "(if #t 1 2)", "1");
   check(read_eqv, "(if #f 1 2)", "2");
-  with_expect_error([]() -> void {
-      check(test_undef, "(if #f 1 2 3)");
-    });
+  check_e_undef("(if #f 1 2 3)");
 
   // syntax: define
   check(read_eqv, "(define x 1)", "1");
@@ -122,22 +100,18 @@ int main(){
   check(read_eqv, "x", "100");
 
   // syntax: begin
-  with_expect_error([]() -> void {
-      check(test_undef, "(begin)");
-    });
+  check_e_undef("(begin)");
   check(read_eqv, "(begin 1)", "1");
   check(read_eqv, "(begin 1 2)", "2");
   check(read_eqv, "(begin 1 2 3)", "3");
 
   // informal syntaxes
-  with_expect_error([]() -> void {
-      //check(test_undef, "else");
-      check(test_undef, "(else)");
-      check(test_undef, "(else 1)");
-      //check(test_undef, "=>");
-      check(test_undef, "(=>)");
-      check(test_undef, "(=> 1)");
-    });
+  //check_e_undef("else");
+  check_e_undef("(else)");
+  check_e_undef("(else 1)");
+  //check_e_undef("=>");
+  check_e_undef("(=>)");
+  check_e_undef("(=> 1)");
 
 
   // macro call
@@ -146,11 +120,11 @@ int main(){
   // function test (basic syntax only)
   check(test_true, "(define fun (lambda (y) (set! x y) (+ y y)))");
   check(test_true, "fun");
-  check(print_equal, "(fun 2)", "4");
-  check(print_equal, "x", "2");
+  check_e("(fun 2)", "4");
+  check_e("x", "2");
     
   check(test_true, "(define (fun2 x) (+ 1 x))");
-  check(print_equal, "(fun2 100)", "101");
+  check_e("(fun2 100)", "101");
 
 
   return (result) ? EXIT_SUCCESS : EXIT_FAILURE;
