e2fc2c4c16473a4b4fc0d588afa3554ea969e8e3
y2q-actionman <y2q.actionman@gmail.com>
2013-01-30 11:25:24 +0900

    moved delay class impl to inline functions

--- src/delay.hh
+++ src/delay.hh
@@ -5,20 +5,24 @@
 
 class Delay{
 public:
-  Delay(Lisp_ptr, Env*);
+  Delay(Lisp_ptr p, Env* e) : expr_(p), env_(e){}
   Delay(const Delay&) = delete;
   Delay(Delay&&) = delete;
 
-  ~Delay();
+  ~Delay() = default;
 
   Delay& operator=(const Delay&) = delete;
   Delay& operator=(Delay&&) = delete;
 
-  bool forced() const { return (env_ == nullptr); }
   Lisp_ptr get() const { return expr_; }
   Env* env() const { return env_; }
 
-  void force(Lisp_ptr);
+  bool forced() const { return (env_ == nullptr); }
+
+  void force(Lisp_ptr p){
+    expr_ = p;
+    env_ = nullptr;
+  }
 
 private:
   Lisp_ptr expr_;
