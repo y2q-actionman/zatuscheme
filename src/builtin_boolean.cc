89214a0a16a33ba536b2516058941291d5fdd15a
y2q <y2q.actionman@gmail.com>
2013-02-10 00:33:59 +0900

    cleanup builtin_boolean.*

--- src/builtin_boolean.cc
+++
@@ -1,6 +0,0 @@
-#include "builtin_boolean.hh"
-#include "lisp_ptr.hh"
-#include "vm.hh"
-#include "builtin_util.hh"
-
-using namespace std;
--- src/builtin_boolean.hh
+++
@@ -1,6 +0,0 @@
-#ifndef BUILTIN_BOOLEAN_HH
-#define BUILTIN_BOOLEAN_HH
-
-#include "decl.hh"
-
-#endif // BUILTIN_BOOLEAN_HH
