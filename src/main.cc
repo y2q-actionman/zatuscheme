584faae79d88032a7de9c5be95f0aa2c8214a7f2
y2q-actionman <y2q.actionman@gmail.com>
2013-04-17 17:49:20 +0900

    changed startup

--- src/main.cc
+++ src/main.cc
@@ -1,24 +1,24 @@
 #include "zs.hh"
-#include <iostream>
+#include <sstream>
 
 using namespace std;
 
-int main(){
+int main(int argc, const char* argz[]){
+  static const char* default_code[] = {"", "(read-eval-print-loop)"};
+
   zs_init();
 
-#ifndef HARD_REPL
-  vm.code.push_back(make_cons_list({intern(vm.symtable(), "read-eval-print-loop")}));
-  eval();
-#else
-  while(1){
-    cout << ">> " << flush;
-    auto val = read(cin);
-    vm.code.push_back(val);
-    eval();
-    print(cout, vm.return_value[0]);
-    cout << endl;
+  if(argc <= 1){
+    argc = sizeof(default_code) / sizeof(default_code[0]);
+    argz = default_code;
+  }    
+
+  for(int i = 1; i < argc; ++i){
+    istringstream iss(argz[i]);
+    vm.code.push_back(read(iss));
   }
-#endif
-  
+
+  eval();
+
   return 0;
 }
