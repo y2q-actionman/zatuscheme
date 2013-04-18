eb0ac977258327a9769080e2ec6d28696f172695
y2q-actionman <y2q.actionman@gmail.com>
2013-04-18 17:05:04 +0900

    added rational operations

--- src/builtin_numeric.cc
+++ src/builtin_numeric.cc
@@ -35,6 +35,7 @@ zs_error number_type_check_failed(const char* func_name, Lisp_ptr p){
 bool is_numeric_type(Lisp_ptr p){
   auto tag = p.tag();
   return (tag == Ptr_tag::integer
+          || tag == Ptr_tag::rational
           || tag == Ptr_tag::real
           || tag == Ptr_tag::complex);
 }
@@ -44,7 +45,7 @@ bool is_integer_type(Lisp_ptr p){
 }
 
 bool is_rational_type(Lisp_ptr p){
-  return (false) // TODO: add rational!
+  return (p.tag() == Ptr_tag::rational)
     || is_integer_type(p);
 }
 
@@ -74,11 +75,20 @@ int coerce(Lisp_ptr p){
 }
 
 template<>
+Rational coerce(Lisp_ptr p){
+  if(p.tag() == Ptr_tag::rational){
+    return *p.get<Rational*>();
+  }else{
+    return Rational(p.get<int>(), 1);
+  }
+}
+
+template<>
 double coerce(Lisp_ptr p){
   if(p.tag() == Ptr_tag::real){
     return *(p.get<double*>());
   }else{
-    return static_cast<double>(coerce<int>(p));
+    return static_cast<double>(coerce<Rational>(p));
   }
 }
 
@@ -94,6 +104,10 @@ Complex coerce(Lisp_ptr p){
 
 Lisp_ptr wrap_number(int i){
   return {Ptr_tag::integer, i};
+}
+
+Lisp_ptr wrap_number(Rational q){
+  return {new Rational(q)};
 }
 
 Lisp_ptr wrap_number(double d){
@@ -116,9 +130,9 @@ Lisp_ptr wrap_number(Lisp_ptr p){
 // implementation utilities
 //
 
-template<typename IFun, typename RFun, typename CFun>
-Lisp_ptr number_unary(Lisp_ptr arg1,
-                      const char* name, const IFun& ifun,
+template<typename IFun, typename QFun, typename RFun, typename CFun>
+Lisp_ptr number_unary(Lisp_ptr arg1, const char* name,
+                      const IFun& ifun, const QFun& qfun,
                       const RFun& rfun, const CFun& cfun){
   if(!is_numeric_type(arg1)){
     throw number_type_check_failed(name, arg1);
@@ -126,6 +140,8 @@ Lisp_ptr number_unary(Lisp_ptr arg1,
 
   if(is_integer_type(arg1)){
     return wrap_number(ifun(coerce<int>(arg1)));
+  }else if(is_rational_type(arg1)){
+    return wrap_number(qfun(coerce<Rational>(arg1)));
   }else if(is_real_type(arg1)){
     return wrap_number(rfun(coerce<double>(arg1)));
   }else if(is_complex_type(arg1)){
@@ -374,6 +390,7 @@ Lisp_ptr number_minus(ZsArgs args){
   if(args.size() == 1){
     return number_unary(args[0], "-",
                         integer_overflow_check_unary<std::negate>(),
+                        negate<Rational>(),
                         negate<double>(),
                         negate<Complex>());
   }else{
@@ -397,6 +414,7 @@ Lisp_ptr number_divide(ZsArgs args){
                             ? wrap_number(1)
                             : wrap_number(1.0 / i);
                         },
+                        [](Rational q){ return Rational(q.denominator, q.numerator); },
                         [](double d){ return 1.0 / d; },
                         [](Complex z){ return 1.0 / z; });
   }else{
@@ -490,6 +508,7 @@ Lisp_ptr number_denominator(ZsArgs args){
 Lisp_ptr number_floor(ZsArgs args){
   return number_unary(args[0], "floor",
                       pass_through(),
+                      [](Rational r){ return std::floor(static_cast<double>(r));},
                       [](double d){ return std::floor(d);},
                       inacceptable_number_type());
 }
@@ -497,6 +516,7 @@ Lisp_ptr number_floor(ZsArgs args){
 Lisp_ptr number_ceil(ZsArgs args){
   return number_unary(args[0], "ceiling",
                       pass_through(),
+                      [](Rational r){ return std::ceil(static_cast<double>(r));},
                       [](double d){ return std::ceil(d);},
                       inacceptable_number_type());
 }
@@ -504,6 +524,7 @@ Lisp_ptr number_ceil(ZsArgs args){
 Lisp_ptr number_trunc(ZsArgs args){
   return number_unary(args[0], "truncate",
                       pass_through(),
+                      [](Rational r){ return std::trunc(static_cast<double>(r));},
                       [](double d){ return std::trunc(d);},
                       inacceptable_number_type());
 }
@@ -511,6 +532,7 @@ Lisp_ptr number_trunc(ZsArgs args){
 Lisp_ptr number_round(ZsArgs args){
   return number_unary(args[0], "round",
                       pass_through(),
+                      [](Rational r){ return std::round(static_cast<double>(r));},
                       [](double d){ return std::round(d);},
                       inacceptable_number_type());
 }
@@ -530,6 +552,7 @@ Lisp_ptr number_rationalize(ZsArgs args){
 Lisp_ptr number_exp(ZsArgs args){
   return number_unary(args[0], "exp",
                       [](int i) -> double { return std::exp(i);},
+                      [](Rational q) -> double { return std::exp(static_cast<double>(q));},
                       [](double d){ return std::exp(d);},
                       [](Complex z){ return std::exp(z);});
 }
@@ -537,6 +560,7 @@ Lisp_ptr number_exp(ZsArgs args){
 Lisp_ptr number_log(ZsArgs args){
   return number_unary(args[0], "log",
                       [](int i) -> double { return std::log(i);},
+                      [](Rational q) -> double { return std::log(static_cast<double>(q));},
                       [](double d){ return std::log(d);},
                       [](Complex z){ return std::log(z);});
 }
@@ -544,6 +568,7 @@ Lisp_ptr number_log(ZsArgs args){
 Lisp_ptr number_sin(ZsArgs args){
   return number_unary(args[0], "sin",
                       [](int i) -> double { return std::sin(i);},
+                      [](Rational q) -> double { return std::sin(static_cast<double>(q));},
                       [](double d){ return std::sin(d);},
                       [](Complex z){ return std::sin(z);});
 }
@@ -551,6 +576,7 @@ Lisp_ptr number_sin(ZsArgs args){
 Lisp_ptr number_cos(ZsArgs args){
   return number_unary(args[0], "cos",
                       [](int i) -> double { return std::cos(i);},
+                      [](Rational q) -> double { return std::cos(static_cast<double>(q));},
                       [](double d){ return std::cos(d);},
                       [](Complex z){ return std::cos(z);});
 }
@@ -558,6 +584,7 @@ Lisp_ptr number_cos(ZsArgs args){
 Lisp_ptr number_tan(ZsArgs args){
   return number_unary(args[0], "tan",
                       [](int i) -> double { return std::tan(i);},
+                      [](Rational q) -> double { return std::tan(static_cast<double>(q));},
                       [](double d){ return std::tan(d);},
                       [](Complex z){ return std::tan(z);});
 }
@@ -565,6 +592,7 @@ Lisp_ptr number_tan(ZsArgs args){
 Lisp_ptr number_asin(ZsArgs args){
   return number_unary(args[0], "asin",
                       [](int i) -> double { return std::asin(i);},
+                      [](Rational q) -> double { return std::asin(static_cast<double>(q));},
                       [](double d){ return std::asin(d);},
                       [](Complex z){ return std::asin(z);});
 }
@@ -572,6 +600,7 @@ Lisp_ptr number_asin(ZsArgs args){
 Lisp_ptr number_acos(ZsArgs args){
   return number_unary(args[0], "acos",
                       [](int i) -> double { return std::acos(i);},
+                      [](Rational q) -> double { return std::acos(static_cast<double>(q));},
                       [](double d){ return std::acos(d);},
                       [](Complex z){ return std::acos(z);});
 }
@@ -581,6 +610,7 @@ Lisp_ptr number_atan(ZsArgs args){
   case 1:  // std::atan()
     return number_unary(args[0], "atan",
                         [](int i) -> double { return std::atan(i); },
+                        [](Rational q) -> double { return std::atan(static_cast<double>(q));},
                         [](double d){ return std::atan(d); },
                         [](Complex z){ return std::atan(z); });
   case 2: // std::atan2()
@@ -600,6 +630,7 @@ Lisp_ptr number_atan(ZsArgs args){
 Lisp_ptr number_sqrt(ZsArgs args){
   return number_unary(args[0], "sqrt",
                       [](int i) -> double { return std::sqrt(i);},
+                      [](Rational q) -> double { return std::sqrt(static_cast<double>(q));},
                       [](double d){ return std::sqrt(d);},
                       [](Complex z){ return std::sqrt(z);});
 }
@@ -646,11 +677,13 @@ Lisp_ptr number_real(ZsArgs args){
   return number_unary(args[0], "real-part",
                       inacceptable_number_type(),
                       inacceptable_number_type(),
+                      inacceptable_number_type(),
                       [](Complex z){ return z.real();});
 }
 
 Lisp_ptr number_imag(ZsArgs args){
   return number_unary(args[0], "imag-part",
+                      inacceptable_number_type(),
                       inacceptable_number_type(),
                       inacceptable_number_type(),
                       [](Complex z){ return z.imag();});
@@ -660,11 +693,13 @@ Lisp_ptr number_mag(ZsArgs args){
   return number_unary(args[0], "magnitude",
                       inacceptable_number_type(),
                       inacceptable_number_type(),
+                      inacceptable_number_type(),
                       [](Complex z){ return std::abs(z);});
 }
 
 Lisp_ptr number_angle(ZsArgs args){
   return number_unary(args[0], "angle",
+                      inacceptable_number_type(),
                       inacceptable_number_type(),
                       inacceptable_number_type(),
                       [](Complex z){ return arg(z);});
@@ -675,6 +710,7 @@ Lisp_ptr number_i_to_e(ZsArgs args){
   // MEMO: add complex<int> type??
   return number_unary(args[0], "inexact->exact",
                       pass_through(),
+                      pass_through(),
                       [](double d){ return static_cast<int>(d);},
                       inacceptable_number_type());
 }
@@ -682,6 +718,7 @@ Lisp_ptr number_i_to_e(ZsArgs args){
 Lisp_ptr number_e_to_i(ZsArgs args){
   return number_unary(args[0], "exact->inexact",
                       [](int i){ return static_cast<double>(i);},
+                      [](Rational q){ return static_cast<double>(q);},
                       pass_through(),
                       pass_through());
 }
@@ -716,6 +753,8 @@ Lisp_ptr number_from_string(ZsArgs args){
 
   if(t.type() == Token::Type::integer){
     return Lisp_ptr(Ptr_tag::integer, t.get<int>());
+  }else if(t.type() == Token::Type::rational){
+    return {new double(t.get<Rational>())};
   }else if(t.type() == Token::Type::real){
     return {new double(t.get<double>())};
   }else if(t.type() == Token::Type::complex){
