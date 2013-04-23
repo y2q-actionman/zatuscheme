900001b12eff8fd4e0794af8efa7eea0f5f416b5
y2q-actionman <y2q.actionman@gmail.com>
2013-04-23 12:09:14 +0900

    added deletor

--- src/zs_memory.hh
+++ src/zs_memory.hh
@@ -2,6 +2,7 @@
 #define ZS_MEMORY_HH
 
 #include <new>
+#include <memory>
 #include "decl.hh"
 
 void* operator new(size_t, Ptr_tag);
@@ -9,5 +10,14 @@ void* operator new[](size_t, Ptr_tag) = delete;
 
 void operator delete(void*, Ptr_tag);
 void operator delete[](void*, Ptr_tag) = delete;
+
+// like 'std::default_delete'
+template<typename T>
+struct zs_memory_delete{
+  void operator()(T* p){
+    p->~T();
+    operator delete(p, to_tag<T>()); 
+  }
+};
 
 #endif // ZS_MEMORY_HH
