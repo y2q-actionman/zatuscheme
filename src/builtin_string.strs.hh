c1d8857165ede9e040ccb0bce71ce4eacfa0899e
y2q <y2q.actionman@gmail.com>
2013-05-20 01:19:38 +0900

    bitly refactoring at string compare

--- src/builtin_string.strs.hh
+++ src/builtin_string.strs.hh
@@ -1,34 +1,24 @@
 // This file is intended to be included into an array of 'const char*'
 
-"(define (string=? s1 s2)"
-"  (= (%string-strcmp s1 s2) 0))",
+#define STRING_CMP_FUNCS(op)                    \
+  "(define (string"op"? s1 s2)"                 \
+  "  ("op" (%string-strcmp s1 s2) 0))"
+STRING_CMP_FUNCS("="),
+STRING_CMP_FUNCS("<"),
+STRING_CMP_FUNCS(">"),
+STRING_CMP_FUNCS("<="),
+STRING_CMP_FUNCS(">="),
+#undef STRING_CMP_FUNCS
 
-"(define (string<? s1 s2)"
-"  (< (%string-strcmp s1 s2) 0))",
-
-"(define (string>? s1 s2)"
-"  (> (%string-strcmp s1 s2) 0))",
-
-"(define (string<=? s1 s2)"
-"  (<= (%string-strcmp s1 s2) 0))",
-
-"(define (string>=? s1 s2)"
-"  (>= (%string-strcmp s1 s2) 0))",
-
-"(define (string-ci=? s1 s2)"
-"  (= (%string-strcasecmp s1 s2) 0))",
-
-"(define (string-ci<? s1 s2)"
-"  (< (%string-strcasecmp s1 s2) 0))",
-
-"(define (string-ci>? s1 s2)"
-"  (> (%string-strcasecmp s1 s2) 0))",
-
-"(define (string-ci<=? s1 s2)"
-"  (<= (%string-strcasecmp s1 s2) 0))",
-
-"(define (string-ci>=? s1 s2)"
-"  (>= (%string-strcasecmp s1 s2) 0))",
+#define STRING_CI_CMP_FUNCS(op)                    \
+  "(define (string-ci"op"? s1 s2)"                 \
+  "  ("op" (%string-strcasecmp s1 s2) 0))"
+STRING_CI_CMP_FUNCS("="),
+STRING_CI_CMP_FUNCS("<"),
+STRING_CI_CMP_FUNCS(">"),
+STRING_CI_CMP_FUNCS("<="),
+STRING_CI_CMP_FUNCS(">="),
+#undef STRING_CI_CMP_FUNCS
 
 "(define (string-copy str)"
 "  (let* ((size (string-length str))"
